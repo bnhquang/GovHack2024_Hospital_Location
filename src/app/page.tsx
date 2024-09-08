fb5528994b9c7b7732d7983bc351d11727fc5754
Andy Bui <bnhquang127@gmail.com>
2024-09-08 16:44:03 +1000

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,168 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { useState, useEffect } from "react";
+import {
+    Checkbox,
+    Autocomplete,
+    FormControlLabel,
+    Grid2 as Grid,
+    TextField,
+} from "@mui/material";
+import { Divider, Input, Table } from "antd";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+const featureList = [
+    "Standardised death rate",
+    "Hospital Prone (0-10 and >= 75)",
+    "Population",
+    "time_to_nearest_hos",
+];
+
+export default function RankComponent() {
+    const [rankedData, setRankedData] = useState(null);
+    const [selectedFeatures, setSelectedFeatures] = useState([]);
+    const [maximize, setMaximize] = useState(
+        Array(selectedFeatures.length).fill(false)
+    );
+    const [council, setCouncil] = useState("");
+
+    useEffect(() => {
+        console.log(maximize);
+    }, [maximize]);
+
+    const handleCouncilChange = (event) => {
+        setCouncil(event.target.value); // Update the council state with input value
+    };
+
+    const handleFeatureChange = (event, value) => {
+        setSelectedFeatures(value);
+
+        // Update maximize array to match the new length of selectedFeatures
+        setMaximize((prevMaximize) => {
+            const newMaximize = [...prevMaximize];
+            // For each new feature added, initialize the corresponding maximize value to false
+            value.forEach((feature, index) => {
+                if (newMaximize[index] === undefined) {
+                    newMaximize[index] = false;
+                }
+            });
+            return newMaximize.slice(0, value.length); // Ensure maximize array matches the selected features length
+        });
+    };
+
+    const handleMaximizeChange = (index) => {
+        // Toggle the maximize state for the feature at the given index
+        const newMaximize = [...maximize];
+        newMaximize[index] = !newMaximize[index];
+        setMaximize(newMaximize);
+    };
+
+    // Function to call Flask API
+    const getRankedData = async () => {
+        const requestData = {
+            features: selectedFeatures,
+            maximize: maximize,
+            weights: Array(selectedFeatures.length).fill(0.25),
+            council: council, // "Hobsons Bay"
+        };
+
+        console.log("requestData", requestData);
+
+        try {
+            const response = await fetch("http://127.0.0.1:5000/rank", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(requestData),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setRankedData(data); // Save the result in state
+            } else {
+                console.error("Error:", response.statusText);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+    // Define the columns for Ant Design Table
+    const columns = [
+        {
+            title: "Rank",
+            dataIndex: "Rank",
+            key: "rank",
+        },
+        {
+            title: "SA2 Code",
+            dataIndex: "SA2 Code",
+            key: "sa2Code",
+        },
+        {
+            title: "Area",
+            dataIndex: "Area",
+            key: "area",
+        },
+    ];
+
+    return (
+        <div>
+            <Autocomplete
+                multiple
+                id="tags-outlined"
+                options={featureList}
+                getOptionLabel={(option) => option}
+                // defaultValue={[featureList[13]]}
+                filterSelectedOptions
+                onChange={handleFeatureChange}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Select Features"
+                        placeholder="Features"
+                    />
+                )}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+            <Input
+                size="large"
+                placeholder="Council"
+                className="mt-4"
+                value={council}
+                onChange={handleCouncilChange}
+            />
+
+            <Grid container spacing={2}>
+                {selectedFeatures.map((feature, index) => (
+                    <Grid item key={feature}>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={maximize[index] || false} // Set checked based on maximize state
+                                    onChange={() => handleMaximizeChange(index)} // Handle checkbox toggle
+                                />
+                            }
+                            label={`Maximize ${feature}`}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+
+            <button onClick={getRankedData}>Get Ranked Data</button>
+
+            {rankedData && (
+                <>
+                    <Divider>Most Suitable Suburbs to Build a Hospital</Divider>
+                    <Table
+                        columns={columns}
+                        dataSource={rankedData.map((row, index) => ({
+                            ...row,
+                            key: index, // Add unique key for each row
+                        }))}
+                        size="middle"
+                    />
+                </>
+            )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
